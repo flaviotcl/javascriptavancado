099e05949680e36fea4cb3583f2557db4dcb01dd
Flavio tavares <tavares.pmsg@gmail.com>
2020-04-14 23:03:13 -0300

    Deletando Dados

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function setList(list){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>'
 
     for(var key in list){
-        table += '<tr><td>'+ formatDesc(list[key].description) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button class="btn btn-default" onclick="setUpdate('+key+')">Edit </button>| Delete</td></tr>'
+        table += '<tr><td>'+ formatDesc(list[key].description) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button class="btn btn-default" onclick="setUpdate('+key+')">Edit </button> <button class="btn btn-default" onclick="deleteItem('+key+')">Delete</button></td></tr>'
     }
 
     table += '</tbody>'
@@ -76,6 +76,23 @@ function updateData(){
     
     resetForm()
     setList(list)
+}
+
+function deleteItem(id){
+    if(confirm('Delete this item ?')){
+        if(id === list.length - 1){
+            list.pop()
+        } else if( id === 0){
+            list.shift()
+        } else{
+            var arrAuxIni = list.slice(0,id) // Retorna do item 0 do array até a posição anterior a do Id.
+            var arrAuxEnd = list.slice(id + 1) // Retorna do próximo item após ao Id até o último item do Array de objetos.
+            list =  arrAuxIni.concat(arrAuxEnd)
+
+        }
+        setList(list)
+    }
+
 }
 var button = document.getElementById('btnAdd')
 button.addEventListener('click', addData)
